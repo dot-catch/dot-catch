f1a4e27d56363177de1a2614f59e7da242acec44
null267 <eigenheiter@gmail.com>
2020-01-27 18:30:50 -0500

    Issues with addProfile in ProfileController resolved

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -2,32 +2,48 @@ const db = require ('../models/model.js');
 
 const profileController = {};
 
-// profileController.addProfile = (req, res, next) => {
-//     console.log('add controller');
-//     const fields = ['login','user_id','avatar_url','followers','name','public_repos','repos_url'];
-//     const values = [];
-//     for (let i = 0; i<fields.length;i+=1) {
-//         if (req.body[fields[i]]) {
-//             // console.log(fields[i], ': ', req.body[fields[i]]);
-//             values.push(req.body[fields[i]]);
-//         } else values.push('');
-//     }
-//     values.push(req.body.login);
-//     // using separate variables for inputs to avoid risk of SQL injection attacks
-//     const text = 'INSERT INTO profiles(login, user_id, avatar_url, followers, name, public_repos, repos_url) VALUES($1, $2, $3, $4, $5, $6, $7) WHERE EXISTS(SELECT 1 FROM profiles WHERE profiles.login = $8)';
-//     const query = { 
-//         name: 'add-profile',
-//         text: text,
-//         values, values
-//     }
-//     db.query(query, (err, profile) => {
-//         if (err) {
-//             return next(err);
-//         } else {
-//             return next();
-//         }
-//     });
-// }
+profileController.addProfile = (req, res, next) => {
+    console.log('add controller');
+    const login = [req.body.login];
+    // console.log(login);
+    const checkQuery = {
+        name: 'profile-check',
+        text: 'SELECT * FROM profiles WHERE login = $1',
+        values: login
+    }
+    // console.log(checkQuery);
+    // res.send(checkQuery);
+    db.query(checkQuery, (err, profile) => {  //if query returns anything, that means that a profile for the user already exists
+        if (profile.rows.length > 0) {
+            // console.log(profile);
+            res.send('Profile already exists');
+        } else {
+            const fields = ['login','user_id','avatar_url','followers','name','public_repos','repos_url'];
+            const values = [];
+            for (let i = 0; i<fields.length;i+=1) {
+                if (req.body[fields[i]]) {
+                    // console.log(fields[i], ': ', req.body[fields[i]]);
+                    values.push(req.body[fields[i]]);
+                } else values.push('');
+            }
+            // using separate variables for inputs to avoid risk of SQL injection attacks
+            const text = 'INSERT INTO profiles(login, user_id, avatar_url, followers, name, public_repos, repos_url) VALUES($1, $2, $3, $4, $5, $6, $7)';
+            const query = { 
+                name: 'add-profile',
+                text: text,
+                values: values
+            }
+            db.query(query, (err, profile) => {
+                if (err) {
+                    return next(err);
+                } else {
+                    res.send('User added')
+                    return next();
+                }
+            });
+        }
+    })
+},
 
 profileController.getAllProfiles = (req, res, next) => {
     console.log('Get All Controller');
@@ -38,7 +54,7 @@ profileController.getAllProfiles = (req, res, next) => {
         text: text,
         values: values
     }
-    db.query(text, values, (err, profiles) => {
+    db.query(query, (err, profiles) => {
         if (err) {
             return next(err);
         } else {

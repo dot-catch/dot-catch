bd5546961054e1ec9e208db0f627910ed07f959f
Charles Finocchiaro <56731501+null267@users.noreply.github.com>
2020-03-19 18:17:23 -0400

    Update model.js

--- models/model.js
+++ models/model.js
@@ -1,7 +1,6 @@
 const { Pool } = require("pg");  //This is all pretty much boilerplate, other than the URI
 
-const PG_URI =
-  "postgres://hbyhdnmz:8wUFtgGrJesBTmJ5qNw--lQvg4k_cmoM@rajje.db.elephantsql.com:5432/hbyhdnmz";
+const PG_URI ="";
 
 const pool = new Pool({
   connectionString: PG_URI
